0eb3e5d954b30f22d2b6073074697c5a15a60b4b
mbme <stribog.ua@gmail.com>
2018-12-07 19:33:48 +0200

    update deps

--- stencil.config.ts
+++ stencil.config.ts
@@ -3,6 +3,7 @@ import { Config } from '@stencil/core';
 // https://stenciljs.com/docs/config
 
 const APP_DIR = 'src/web-app'
+
 export const config: Config = {
   globalStyle: `${APP_DIR}/global/app.css`,
   globalScript: `${APP_DIR}/global/app.ts`,

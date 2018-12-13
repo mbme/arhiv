45da1acebf8b01315c6581b2c19c3e108a003b7b
mbme <stribog.ua@gmail.com>
2018-12-13 17:40:26 +0200

    add rollup & inferno

---
+++ rollup.config.js
@@ -0,0 +1,27 @@
+import replace from 'rollup-plugin-replace';
+import typescript from 'rollup-plugin-typescript';
+import { terser } from "rollup-plugin-terser";
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+export default {
+  input: 'src/web-client/index.jsx',
+
+  output: {
+    file: 'dist/bundle.js',
+    format: 'iife',
+    sourcemap: true,
+  },
+
+  plugins: [
+    typescript(),
+
+    replace({
+      'process.env.NODE_ENV': JSON.stringify('production'),
+      'process.env.LOG': JSON.stringify(process.env.LOG),
+      __SERVER__: JSON.stringify(false),
+    }),
+
+    isProduction && terser(),
+  ],
+};

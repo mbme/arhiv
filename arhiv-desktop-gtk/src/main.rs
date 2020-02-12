c4a1b588a3941f36971dacc9aee3c50251a4a52f
mbme <stribog.ua@gmail.com>
2020-02-12 21:34:37 +0200

    gtk webview example

---
+++ arhiv-desktop-gtk/src/main.rs
@@ -0,0 +1,33 @@
+extern crate gio;
+extern crate gtk;
+extern crate webkit2gtk;
+
+use gio::prelude::*;
+use gtk::prelude::*;
+use gtk::{Application, ApplicationWindow};
+use webkit2gtk::{SettingsExt, WebView, WebViewExt};
+
+fn main() {
+    let application =
+        Application::new(Some("com.github.gtk-rs.examples.basic"), Default::default())
+            .expect("failed to initialize GTK application");
+
+    application.connect_activate(|app| {
+        let window = ApplicationWindow::new(app);
+        window.set_title("First GTK+ Program");
+        window.set_default_size(350, 70);
+
+        // https://docs.rs/webkit2gtk/0.8.0/webkit2gtk/trait.WebViewExt.html
+        let webview = WebView::new();
+        webview.load_uri("https://crates.io/");
+
+        let settings = WebViewExt::get_settings(&webview).unwrap();
+        settings.set_enable_developer_extras(true);
+
+        window.add(&webview);
+
+        window.show_all();
+    });
+
+    application.run(&[]);
+}

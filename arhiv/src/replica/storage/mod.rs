378bba694a917f3d5a1146dba30b6e5cfcb51526
mbme <stribog.ua@gmail.com>
2020-02-26 21:09:38 +0200

    extract path management

--- arhiv/src/replica/storage/mod.rs
+++ arhiv/src/replica/storage/mod.rs
@@ -1,28 +1,30 @@
 use crate::entities::*;
 use crate::utils::ensure_exists;
 use anyhow::*;
+use pathfinder::PathFinder;
 use state::StorageState;
 use std::fs;
 use std::path::Path;
 use std::str::FromStr;
 
+mod pathfinder;
 mod state;
 
 pub struct Storage {
-    root_path: String,
+    pf: PathFinder,
 }
 
 impl Storage {
     pub fn open(path: &str) -> Result<Storage> {
         let replica = Storage {
-            root_path: path.to_string(),
+            pf: PathFinder::new(path),
         };
 
-        ensure_exists(&replica.root_path, true)?;
-        ensure_exists(&replica.get_state_file(), false)?;
-        ensure_exists(&replica.get_documents_directory(), true)?;
-        ensure_exists(&replica.get_attachments_directory(), true)?;
-        ensure_exists(&replica.get_attachments_data_directory(), true)?;
+        ensure_exists(&replica.pf.root_path, true)?;
+        ensure_exists(&replica.pf.get_state_file(), false)?;
+        ensure_exists(&replica.pf.get_documents_directory(), true)?;
+        ensure_exists(&replica.pf.get_attachments_directory(), true)?;
+        ensure_exists(&replica.pf.get_attachments_data_directory(), true)?;
 
         // TODO lock file
 
@@ -41,56 +43,29 @@ impl Storage {
         }
 
         let replica = Storage {
-            root_path: path_str.to_string(),
+            pf: PathFinder::new(path_str),
         };
 
         // create required dirs
-        fs::create_dir(&replica.root_path)?;
-        fs::create_dir(&replica.get_documents_directory())?;
-        fs::create_dir(&replica.get_attachments_directory())?;
-        fs::create_dir(&replica.get_attachments_data_directory())?;
+        fs::create_dir(&replica.pf.root_path)?;
+        fs::create_dir(&replica.pf.get_documents_directory())?;
+        fs::create_dir(&replica.pf.get_attachments_directory())?;
+        fs::create_dir(&replica.pf.get_attachments_data_directory())?;
 
         // create state file
-        StorageState::new().write(&replica.get_state_file())?;
+        StorageState::new().write(&replica.pf.get_state_file())?;
 
         println!("created arhiv replica in {}", path_str);
 
         Ok(replica)
-    }
-
-    fn get_state_file(&self) -> String {
-        format!("{}/arhiv-state.json", self.root_path)
-    }
-
-    fn get_documents_directory(&self) -> String {
-        format!("{}/documents", self.root_path)
-    }
-
-    fn get_attachments_directory(&self) -> String {
-        format!("{}/attachments", self.root_path)
-    }
-
-    fn get_attachments_data_directory(&self) -> String {
-        format!("{}/attachments-data", self.root_path)
-    }
-
-    fn get_document_path(&self, id: &Id) -> String {
-        format!("{}/{}.json", self.get_documents_directory(), id)
-    }
-
-    fn get_attachment_path(&self, id: &Id) -> String {
-        format!("{}/{}.json", self.get_attachments_directory(), id)
-    }
-
-    pub fn get_attachment_data_path(&self, id: &Id) -> String {
-        format!("{}/{}", self.get_attachments_data_directory(), id)
     }
 }
 
 impl Storage {
     pub fn get_state(&self) -> StorageState {
         // TODO lazy
-        StorageState::read(&self.get_state_file()).expect("must be able to read replica state file")
+        StorageState::read(&self.pf.get_state_file())
+            .expect("must be able to read replica state file")
     }
 
     pub fn get_rev(&self) -> Revision {
@@ -108,8 +83,12 @@ impl Storage {
         state.replica_rev = new_rev;
 
         state
-            .write(&self.get_state_file())
+            .write(&self.pf.get_state_file())
             .expect("must be able to write replica state file");
+    }
+
+    pub fn get_attachment_data_path(&self, id: &Id) -> String {
+        self.pf.get_attachment_data_path(id)
     }
 }
 
@@ -141,32 +120,35 @@ impl Storage {
     }
 
     pub fn get_documents(&self) -> Vec<Document> {
-        self.get_items(&self.get_documents_directory())
+        self.get_items(&self.pf.get_documents_directory())
     }
 
     pub fn get_attachments(&self) -> Vec<Attachment> {
-        self.get_items(&self.get_attachments_directory())
+        self.get_items(&self.pf.get_attachments_directory())
     }
 
     pub fn get_document(&self, id: &Id) -> Option<Document> {
-        self.get_item(&self.get_document_path(id))
+        self.get_item(&self.pf.get_document_path(id))
     }
 
     pub fn get_attachment(&self, id: &Id) -> Option<Attachment> {
-        self.get_item(&self.get_attachment_path(id))
+        self.get_item(&self.pf.get_attachment_path(id))
     }
 }
 
 impl Storage {
     pub fn put_document(&self, document: &Document) -> Result<()> {
-        fs::write(self.get_document_path(&document.id), document.serialize())?;
+        fs::write(
+            self.pf.get_document_path(&document.id),
+            document.serialize(),
+        )?;
 
         Ok(())
     }
 
     pub fn put_attachment(&self, attachment: &Attachment) -> Result<()> {
         fs::write(
-            self.get_attachment_path(&attachment.id),
+            self.pf.get_attachment_path(&attachment.id),
             attachment.serialize(),
         )?;
 
@@ -174,7 +156,7 @@ impl Storage {
     }
 
     pub fn put_attachment_data(&self, id: &Id, src: &str, move_file: bool) -> Result<()> {
-        let dst = self.get_attachment_data_path(id);
+        let dst = self.pf.get_attachment_data_path(id);
 
         if move_file {
             fs::rename(src, dst)?;
@@ -186,7 +168,7 @@ impl Storage {
     }
 
     pub fn remove_attachment_data(&self, id: &Id) -> Result<()> {
-        fs::remove_file(self.get_attachment_data_path(id))?;
+        fs::remove_file(self.pf.get_attachment_data_path(id))?;
 
         Ok(())
     }
---
+++ arhiv/src/replica/storage/pathfinder.rs
@@ -0,0 +1,41 @@
+use crate::entities::*;
+
+pub struct PathFinder {
+    pub root_path: String,
+}
+
+impl PathFinder {
+    pub fn new(root_path: &str) -> PathFinder {
+        PathFinder {
+            root_path: root_path.to_string(),
+        }
+    }
+
+    pub fn get_state_file(&self) -> String {
+        format!("{}/arhiv-state.json", self.root_path)
+    }
+
+    pub fn get_documents_directory(&self) -> String {
+        format!("{}/documents", self.root_path)
+    }
+
+    pub fn get_attachments_directory(&self) -> String {
+        format!("{}/attachments", self.root_path)
+    }
+
+    pub fn get_attachments_data_directory(&self) -> String {
+        format!("{}/attachments-data", self.root_path)
+    }
+
+    pub fn get_document_path(&self, id: &Id) -> String {
+        format!("{}/{}.json", self.get_documents_directory(), id)
+    }
+
+    pub fn get_attachment_path(&self, id: &Id) -> String {
+        format!("{}/{}.json", self.get_attachments_directory(), id)
+    }
+
+    pub fn get_attachment_data_path(&self, id: &Id) -> String {
+        format!("{}/{}", self.get_attachments_data_directory(), id)
+    }
+}

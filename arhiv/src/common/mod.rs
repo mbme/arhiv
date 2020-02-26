5afd61ccac9c18896b28de901d1647660221865f
mbme <stribog.ua@gmail.com>
2020-02-26 22:22:05 +0200

    shared storage

--- arhiv/src/common/mod.rs
+++
@@ -1,5 +0,0 @@
-mod pathfinder;
-mod state;
-
-pub use pathfinder::PathFinder;
-pub use state::*;
--- arhiv/src/replica/storage.rs
+++ arhiv/src/replica/storage.rs
@@ -1,52 +1,9 @@
-use crate::common::{PathFinder, StateDTO, StorageState};
 use crate::entities::*;
+pub use crate::storage::{StateDTO, Storage};
 use anyhow::*;
 use std::fs;
 use std::path::Path;
 use std::str::FromStr;
-
-pub struct Storage {
-    pf: PathFinder,
-    state: StorageState,
-}
-
-impl Storage {
-    pub fn open(root_path: &str) -> Result<Storage> {
-        let pf = PathFinder::new(root_path.to_string());
-        pf.assert_dirs_exist()?;
-
-        let state = StorageState::new(root_path);
-        state.asset_exists()?;
-
-        // TODO lock file
-
-        Ok(Storage { pf, state })
-    }
-
-    pub fn create(root_path: &str) -> Result<Storage> {
-        let path = Path::new(root_path);
-
-        if !path.is_absolute() {
-            return Err(anyhow!("path must be absolute: {}", root_path));
-        }
-
-        if path.exists() {
-            return Err(anyhow!("path already exists: {}", root_path));
-        }
-
-        let pf = PathFinder::new(root_path.to_string());
-        pf.create_dirs()?; // create required dirs
-
-        let state = StorageState::new(root_path);
-        state.init()?; // create state file
-
-        let replica = Storage { pf, state };
-
-        println!("created arhiv replica in {}", root_path);
-
-        Ok(replica)
-    }
-}
 
 impl Storage {
     pub fn get_rev(&self) -> Revision {

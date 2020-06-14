7e434065f88625754d4530fd5409418fea7b822e
mbme <stribog.ua@gmail.com>
2020-06-14 14:19:35 +0300

    remove unused files

--- ui/build/arhiv.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env bash
-
-set -e
-
-./node_modules/.bin/tsc \
-  -p ./tsconfig.json \
-  --noEmitOnError
-
-./build/node.sh tsdist/src/arhiv/bin $@
--- ui/build/node.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env bash
-
-NODE_NO_WARNINGS=1 node \
-                --experimental-modules \
-                --experimental-loader ./build/loader.js \
-                --es-module-specifier-resolution=node \
-                $@

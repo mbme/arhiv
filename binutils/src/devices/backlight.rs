8e4fb3178b010d26cf37d50404c1d5091f65e576
mbme <stribog.ua@gmail.com>
2020-07-15 20:34:52 +0300

    update utils

--- binutils/src/devices/backlight.rs
+++ binutils/src/devices/backlight.rs
@@ -4,24 +4,16 @@ pub struct Backlight;
 
 impl Backlight {
     pub fn inc() {
-        run_command(
-            "xbacklight",
-            vec!["-inc", "10", "-time", "50", "-steps", "1"],
-        )
-        .expect("must be able to increase backlight");
+        run_command("light", vec!["-A", "10"]).expect("must be able to increase backlight");
     }
 
     pub fn dec() {
-        run_command(
-            "xbacklight",
-            vec!["-dec", "10", "-time", "50", "-steps", "1"],
-        )
-        .expect("must be able to decrease backlight");
+        run_command("light", vec!["-U", "10"]).expect("must be able to decrease backlight");
     }
 
     pub fn print_status(notify: bool) {
-        let output =
-            run_command("xbacklight", vec!["-getf"]).expect("must be able to get backlight");
+        let output = run_command("light", vec!["-G"]).expect("must be able to get backlight");
+        let output = output.trim();
 
         let message = format!("Backlight level: {}%", output);
 

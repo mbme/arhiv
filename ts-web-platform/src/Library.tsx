9f2868834f1b67414947793e281cfea9ceb3157a
mbme <stribog.ua@gmail.com>
2020-09-27 18:52:51 +0300

    render Link examples

--- ts-web-platform/src/Library.tsx
+++ ts-web-platform/src/Library.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 
-import { HotkeysResolverProvider } from '@v/web-utils'
+import { HotkeysResolverProvider, RouterProvider } from '@v/web-utils'
 import { StylishProvider } from './core'
 import { OverlayRenderer } from './Modal'
 import { ButtonExamples } from './Button.examples'
@@ -8,37 +8,42 @@ import { SelectExamples } from './Form/Select.examples'
 import { InputExamples } from './Form/Input.examples'
 import { TextareaExamples } from './Form/Textarea.examples'
 import { IconExamples } from './Icon.examples'
+import { LinkExamples } from './Link.examples'
 import { StyleExamples } from './core/examples'
 import { ConfirmationDialogExamples } from './Modal/ConfirmationDialog.examples'
 import { FocusRegion } from './Focus'
 
 export function Library() {
   return (
-    <StylishProvider>
-      <HotkeysResolverProvider>
-        <FocusRegion
-          name="Global"
-          mode="column"
-        >
-          <div>
-            <OverlayRenderer>
-              <StyleExamples />
+    <RouterProvider hashBased>
+      <StylishProvider>
+        <HotkeysResolverProvider>
+          <FocusRegion
+            name="Global"
+            mode="column"
+          >
+            <div>
+              <OverlayRenderer>
+                <StyleExamples />
 
-              <IconExamples />
+                <IconExamples />
 
-              <ButtonExamples />
+                <ButtonExamples />
 
-              <SelectExamples />
+                <LinkExamples />
 
-              <InputExamples />
+                <SelectExamples />
 
-              <TextareaExamples />
+                <InputExamples />
 
-              <ConfirmationDialogExamples />
-            </OverlayRenderer>
-          </div>
-        </FocusRegion>
-      </HotkeysResolverProvider>
-    </StylishProvider>
+                <TextareaExamples />
+
+                <ConfirmationDialogExamples />
+              </OverlayRenderer>
+            </div>
+          </FocusRegion>
+        </HotkeysResolverProvider>
+      </StylishProvider>
+    </RouterProvider>
   )
 }

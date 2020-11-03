1abdf874d0877e0b0fa4c7310468f572d90c783c
mbme <stribog.ua@gmail.com>
2020-11-03 16:23:23 +0200

    fix multipart parser

--- ts-utils-node/src/http-server/multipart-body-parser/parser-file-body.ts
+++ ts-utils-node/src/http-server/multipart-body-parser/parser-file-body.ts
@@ -4,24 +4,17 @@ import { ParserState } from './parser-state'
 export class ParserFileBody {
   public readonly file: string
 
-  private _ws: fs.WriteStream
-
   constructor(
     private _state: ParserState,
     public readonly name: string,
   ) {
     this.file = _state.genTempFile()
-    this._ws = fs.createWriteStream(this.file)
   }
 
   processChunk(): boolean {
     const [foundBoundary, data] = this._state.consumeTillBoundary()
 
-    this._ws.write(data)
-
-    if (foundBoundary) {
-      this._ws.end()
-    }
+    fs.appendFileSync(this.file, data)
 
     return foundBoundary
   }

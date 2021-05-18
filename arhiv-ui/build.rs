1f3d317986408c4e6d4d3cdb554eda034fc5b1ec
mbme <stribog.ua@gmail.com>
2021-05-18 19:50:33 +0300

    fix ui build

--- arhiv-ui/build.rs
+++ arhiv-ui/build.rs
@@ -1,4 +1,4 @@
-use std::{env, process::Command};
+use std::{env, process};
 
 // build web app in release mode
 fn main() {
@@ -6,13 +6,16 @@ fn main() {
         return;
     }
 
-    let result = Command::new("yarn")
-        .arg("build:release")
+    let exit_status = process::Command::new("yarn")
+        .arg("build:prod")
         .status()
         .expect("failed to build web app");
 
-    println!("cargo:warning=result is {}", result);
-
     println!("cargo:rerun-if-env-changed=PROFILE");
     println!("cargo:rerun-if-changed=src");
+
+    if !exit_status.success() {
+        println!("cargo:warning=exit status is {}", exit_status);
+        process::exit(1);
+    }
 }

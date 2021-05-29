2a149e925808b5fe17324db5357ce70731dd9d99
mbme <stribog.ua@gmail.com>
2021-05-29 13:52:14 +0300

    add pagination to catalog

--- arhiv-ui3/src/catalog_page.rs
+++ arhiv-ui3/src/catalog_page.rs
@@ -1,6 +1,6 @@
 use anyhow::*;
 use chrono::{DateTime, Local};
-use rocket::State;
+use rocket::{uri, State};
 use rocket_contrib::templates::Template;
 use serde::Serialize;
 use serde_json::json;
@@ -8,6 +8,8 @@ use serde_json::json;
 use arhiv::{entities::*, Filter, Matcher, OrderBy};
 
 use crate::utils::AppContext;
+
+const PAGE_SIZE: u8 = 14;
 
 #[derive(Serialize)]
 struct CatalogEntry {
@@ -17,20 +19,29 @@ struct CatalogEntry {
     updated_at: DateTime<Local>,
 }
 
-#[get("/catalogs/<document_type>")]
-pub fn catalog_page(document_type: String, context: State<AppContext>) -> Result<Template> {
+#[get("/catalogs/<document_type>?<page>")]
+pub fn catalog_page(
+    document_type: String,
+    page: Option<u8>,
+    context: State<AppContext>,
+) -> Result<Template> {
+    let page = page.unwrap_or(0);
+
     let mut filter = Filter::default();
     filter.matchers.push(Matcher::Type {
         document_type: document_type.clone(),
     });
-    filter.page_size = Some(12);
+    filter.page_size = Some(PAGE_SIZE);
+    filter.page_offset = Some(PAGE_SIZE * page);
     filter.order.push(OrderBy::UpdatedAt { asc: false });
 
     let renderer = context.get_renderer();
 
-    let page = context
-        .arhiv
-        .list_documents(filter)?
+    let result = context.arhiv.list_documents(filter)?;
+
+    let items: Vec<_> = result
+        .items
+        .into_iter()
         .map(|document| CatalogEntry {
             preview: renderer
                 .get_preview(&document)
@@ -38,13 +49,29 @@ pub fn catalog_page(document_type: String, context: State<AppContext>) -> Result
             id: document.id,
             document_type: document.document_type,
             updated_at: document.updated_at.into(),
-        });
+        })
+        .collect();
+
+    let prev_link = match page {
+        0 => None,
+        1 => Some(uri!(catalog_page: &document_type, _).to_string()),
+        _ => Some(uri!(catalog_page: &document_type, page - 1).to_string()),
+    };
+
+    let next_link = if result.has_more {
+        Some(uri!(catalog_page: &document_type, page + 1).to_string())
+    } else {
+        None
+    };
 
     Ok(Template::render(
         "catalog_page",
         json!({
             "document_type": document_type,
+            "items": items,
+            "prev_link": prev_link,
             "page": page,
+            "next_link": next_link,
         }),
     ))
 }

f926543e48348338378b36eeaec8652d54a0f514
mbme <stribog.ua@gmail.com>
2021-05-31 15:05:42 +0300

    make markup links color the same as global link

--- arhiv-ui3/windi.config.js
+++ arhiv-ui3/windi.config.js
@@ -1,4 +1,18 @@
 module.exports = {
+  theme: {
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            a: {
+              color: null, // use global styles
+              textDecoration: 'none',
+            },
+          },
+        },
+      },
+    },
+  },
   plugins: [
     require('windicss/plugin/line-clamp'),
     require('windicss/plugin/typography'),

21b7dc7bda8ecc3b7b6f15627982953804630769
mbme <stribog.ua@gmail.com>
2021-08-27 11:28:50 +0300

    add more languages

--- arhiv-core/src/definitions/fields.rs
+++ arhiv-core/src/definitions/fields.rs
@@ -1,7 +1,23 @@
 use crate::schema::FieldType;
 
 pub fn language_field() -> FieldType {
-    FieldType::Enum(vec!["Ukrainian", "English", "Russian"])
+    FieldType::Enum(vec![
+        "Ukrainian",
+        "English",
+        "French",
+        "German",
+        "Polish",
+        "Spanish",
+        "Portuguese",
+        "Russian",
+        "Chinese",
+        "Hindi",
+        "Bengali",
+        "Japanese",
+        "Korean",
+        "Turkish",
+        "Arabic",
+    ])
 }
 
 pub fn rating_field() -> FieldType {

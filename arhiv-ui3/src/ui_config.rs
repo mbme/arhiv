75a516b66f65b42dd19183318b58ed71ab188d9c
mbme <stribog.ua@gmail.com>
2021-10-18 13:56:25 +0300

    refactor: use document type variables

--- arhiv-ui3/src/ui_config.rs
+++ arhiv-ui3/src/ui_config.rs
@@ -1,16 +1,18 @@
+use arhiv_core::definitions::{BOOK_TYPE, FILM_TYPE};
+
 use crate::components::CatalogConfig;
 
 pub fn get_catalog_config(document_type: impl AsRef<str>) -> CatalogConfig {
     let document_type = document_type.as_ref();
 
-    if document_type == "book" {
+    if document_type == BOOK_TYPE {
         return CatalogConfig {
             fields: vec!["authors", "rating"],
             preview: None,
         };
     }
 
-    if document_type == "film" {
+    if document_type == FILM_TYPE {
         return CatalogConfig {
             fields: vec!["duration", "release_date", "rating"],
             preview: None,

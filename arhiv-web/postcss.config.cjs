04179b855b6e1d8d1a0245f5d927bb57b03a0cbe
mbme <stribog.ua@gmail.com>
2021-11-16 17:55:36 +0200

    remove arhiv-web

--- arhiv-web/postcss.config.cjs
+++
@@ -1,8 +0,0 @@
-/* eslint-env node */
-module.exports = {
-  plugins: [
-    require('tailwindcss'),
-    require('postcss-nesting'),
-    require('autoprefixer'),
-  ],
-};
--- arhiv-web/src/main.rs
+++
@@ -1,8 +0,0 @@
-use arhiv_web::start_ui_server;
-use rs_utils::log::setup_logger;
-
-#[tokio::main]
-async fn main() {
-    setup_logger();
-    start_ui_server().await;
-}

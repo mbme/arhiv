3b2a756ba068b06598bc6b2066bd88e8addd5995
mbme <stribog.ua@gmail.com>
2021-12-26 14:43:59 +0200

    fix: use CARGO_MANIFEST_DIR in arhiv-import build script

--- arhiv-import/build.rs
+++ arhiv-import/build.rs
@@ -1,6 +1,6 @@
-use std::env;
+use std::{env, path::Path};
 
-use rs_utils::{create_file_if_not_exist, current_dir_relpath, run_yarn};
+use rs_utils::{create_file_if_not_exist, run_yarn};
 
 fn main() {
     println!("cargo:rerun-if-env-changed=PROFILE");
@@ -15,7 +15,7 @@ fn main() {
     }
 
     // in dev mode create file if missing so that CI doesn't fail
-    let file_path = current_dir_relpath("dist/bundle.js");
+    let file_path = Path::new(env!("CARGO_MANIFEST_DIR")).join("dist/bundle.js");
 
     create_file_if_not_exist(file_path).expect("failed to create file if not exist");
 }

548017c3e98e23b977d6412510a9fc864f83242d
mbme <stribog.ua@gmail.com>
2022-07-31 11:24:48 +0300

    scraper: add strict types in rust wrapper

--- scraper/src/lib.rs
+++ scraper/src/lib.rs
@@ -19,12 +19,107 @@ fn get_script_temp_file() -> Result<TempFile> {
 }
 
 #[derive(Serialize, Deserialize)]
+#[serde(deny_unknown_fields)]
+pub struct FacebookPostListItem {
+    permalink: String,
+    date: String,
+    #[serde(rename = "dateISO")]
+    date_iso: Option<String>,
+    content: String,
+    images: Vec<String>,
+    videos: Vec<String>,
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(deny_unknown_fields)]
+pub struct FacebookMobilePostListItem {
+    permalink: String,
+    date: String,
+    #[serde(rename = "dateISO")]
+    date_iso: Option<String>,
+    preview: String,
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(deny_unknown_fields, tag = "typeName")]
+pub enum ScrapedData {
+    FacebookPost {
+        permalink: String,
+        date: String,
+        #[serde(rename = "dateISO")]
+        date_iso: Option<String>,
+        content: String,
+        images: Vec<String>,
+    },
+    FacebookPostList {
+        posts: Vec<FacebookPostListItem>,
+    },
+    FacebookMobilePost {
+        permalink: String,
+        date: String,
+        #[serde(rename = "dateISO")]
+        date_iso: Option<String>,
+        content: String,
+    },
+    FacebookMobilePostList {
+        posts: Vec<FacebookMobilePostListItem>,
+    },
+    #[serde(rename_all = "camelCase")]
+    IMDBFilm {
+        title: String,
+        #[serde(rename = "coverURL")]
+        cover_url: String,
+        release_date: String,
+        original_language: String,
+        countries_of_origin: String,
+        creators: String,
+        cast: String,
+        seasons: Option<u8>,
+        episodes: Option<u8>,
+        duration: String,
+        description: String,
+    },
+    #[serde(rename_all = "camelCase")]
+    MyAnimeListAnime {
+        title: String,
+        #[serde(rename = "coverURL")]
+        cover_url: String,
+        release_date: String,
+        creators: String,
+        duration: String,
+        description: String,
+    },
+    #[serde(rename_all = "camelCase")]
+    SteamGame {
+        #[serde(rename = "coverURL")]
+        cover_url: String,
+        name: String,
+        release_date: String,
+        developers: String,
+        description: String,
+    },
+    #[serde(rename_all = "camelCase")]
+    YakabooBook {
+        #[serde(rename = "coverURL")]
+        cover_url: String,
+        title: String,
+        authors: String,
+        publication_date: String,
+        description: String,
+        translators: String,
+        publisher: String,
+        pages: u32,
+        language: String,
+    },
+}
+
+#[derive(Serialize, Deserialize)]
 #[serde(deny_unknown_fields, rename_all = "camelCase")]
 pub struct ScrapeResult {
     url: String,
     original_url: Option<String>,
     scraper_name: Option<String>,
-    data: Option<serde_json::Value>,
+    data: Option<ScrapedData>,
     error: Option<String>,
 }
 

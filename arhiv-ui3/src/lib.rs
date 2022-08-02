7c64c0b6b6d3538bbb375a8b8f56ceb722487d63
mbme <stribog.ua@gmail.com>
2022-08-02 11:21:01 +0300

    chore: remove pedantic clippy checks

--- arhiv-ui3/src/lib.rs
+++ arhiv-ui3/src/lib.rs
@@ -1,18 +1,4 @@
 #![deny(clippy::all)]
-#![deny(clippy::pedantic)]
-#![allow(
-    clippy::module_name_repetitions,
-    clippy::module_inception,
-    clippy::wildcard_imports,
-    clippy::missing_errors_doc,
-    clippy::missing_panics_doc,
-    clippy::cast_precision_loss,
-    clippy::cast_possible_truncation,
-    clippy::cast_possible_wrap,
-    clippy::cast_sign_loss,
-    clippy::cast_lossless
-)]
-#![allow(clippy::unused_async)]
 
 use hyper::Server;
 

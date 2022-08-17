2d73a01b9917575f3065ba300901b5a913b34b36
mbme <stribog.ua@gmail.com>
2022-08-17 11:56:02 +0300

    chore: improve bump-version script

--- bump-version.sh
+++ bump-version.sh
@@ -3,6 +3,8 @@
 CURRENT_VERSION=$(git describe --abbrev=0 --tags)
 NEXT_VERSION=$(($CURRENT_VERSION + 1))
 
+echo "Current version is $CURRENT_VERSION"
+
 read -p "Are you sure you want to create new tag $NEXT_VERSION? (y)" -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]

442ed2ac0a4ebfa7113bc62df76f2680c26728eb
mbme <stribog.ua@gmail.com>
2022-08-17 11:42:41 +0300

    backlight: add keyboard backlight support

--- binutils/src/bin/mb-backlight.rs
+++ binutils/src/bin/mb-backlight.rs
@@ -4,13 +4,17 @@ use clap::{Parser, Subcommand};
 
 use rs_utils::{get_crate_version, log, Backlight};
 
-/// Control brightness of the screen of the laptop
+/// Control brightness of the display or keyboard
 #[derive(Parser, Debug)]
 #[clap(version = get_crate_version(), about, long_about = None, arg_required_else_help = true)]
 struct Args {
     /// Show notification with the current brightness level
     #[clap(short, action, global = true)]
     notify: bool,
+
+    /// Control keyboard brightness
+    #[clap(long, action, global = true)]
+    keyboard: bool,
 
     #[clap(subcommand)]
     command: Command,
@@ -33,17 +37,23 @@ fn main() {
 
     let args = Args::parse();
 
+    let backlight = if args.keyboard {
+        Backlight::keyboard_control()
+    } else {
+        Backlight::display_control()
+    };
+
     match args.command {
         Command::Increase => {
-            Backlight::inc();
-            Backlight::print_status(args.notify);
+            backlight.inc();
+            backlight.print_status(args.notify);
         }
         Command::Decrease => {
-            Backlight::dec();
-            Backlight::print_status(args.notify);
+            backlight.dec();
+            backlight.print_status(args.notify);
         }
         Command::Status => {
-            Backlight::print_status(args.notify);
+            backlight.print_status(args.notify);
         }
     }
 }
--- rs-utils/src/tools/backlight.rs
+++ rs-utils/src/tools/backlight.rs
@@ -1,31 +1,83 @@
+use anyhow::{Context, Result};
+
 use crate::{run_command, send_notification};
 
-pub struct Backlight;
-
-const STEP: f32 = 5.0;
+pub struct Backlight {
+    step: f32,
+    min: f32,
+    max: f32,
+    keyboard_backlight: bool,
+}
 
 impl Backlight {
-    pub fn inc() {
-        let level = Backlight::get_level();
-
-        Backlight::set_level(level + STEP);
+    pub fn display_control() -> Self {
+        Backlight {
+            step: 5.0,
+            min: 1.0,
+            max: 100.0,
+            keyboard_backlight: false,
+        }
     }
 
-    pub fn dec() {
-        let level = Backlight::get_level();
-
-        Backlight::set_level(level - STEP);
+    pub fn keyboard_control() -> Self {
+        Backlight {
+            step: 1.0,
+            min: 0.0,
+            max: 3.0,
+            keyboard_backlight: true,
+        }
     }
 
-    fn set_level(level: f32) {
-        let level: f32 = level.max(1.0).min(100.0);
+    pub fn inc(&self) {
+        let level = self.get_level();
 
-        run_command("light", vec!["-S", &level.to_string()])
+        self.set_level(level + self.step);
+    }
+
+    pub fn dec(&self) {
+        let level = self.get_level();
+
+        self.set_level(level - self.step);
+    }
+
+    fn find_kbd_backlight_device() -> Result<String> {
+        let output = run_command("light", vec!["-L"]).context("failed to list devices")?;
+
+        output
+            .lines()
+            .find_map(|line| {
+                if line.contains("kbd_backlight") {
+                    Some(line.trim().to_string())
+                } else {
+                    None
+                }
+            })
+            .context("Failed to find keyboard backlight devices")
+    }
+
+    fn run_command(&self, args: Vec<&str>) -> Result<String> {
+        let mut args: Vec<String> = args.into_iter().map(|arg| arg.to_string()).collect();
+
+        if self.keyboard_backlight {
+            let device = Backlight::find_kbd_backlight_device()?;
+            args.append(&mut vec!["-s".to_string(), device, "-r".to_string()]);
+        }
+
+        let args: Vec<&str> = args.iter().map(AsRef::as_ref).collect();
+        run_command("light", args).context("must be able to set backlight level")
+    }
+
+    fn set_level(&self, level: f32) {
+        let level: f32 = level.max(self.min).min(self.max);
+
+        self.run_command(vec!["-S", &level.to_string()])
             .expect("must be able to set backlight level");
     }
 
-    fn get_level() -> f32 {
-        let output = run_command("light", vec!["-G"]).expect("must be able to get backlight level");
+    fn get_level(&self) -> f32 {
+        let output = self
+            .run_command(vec!["-G"])
+            .expect("must be able to get backlight level");
         let output = output.trim();
 
         let level: f32 = output
@@ -35,10 +87,19 @@ impl Backlight {
         level.round()
     }
 
-    pub fn print_status(notify: bool) {
-        let level = Backlight::get_level();
+    pub fn print_status(&self, notify: bool) {
+        let level = self.get_level();
 
-        let message = format!("Backlight level: {}%", level);
+        let message = format!(
+            "{} backlight level: {level} [{}-{}]",
+            if self.keyboard_backlight {
+                "Keyboard"
+            } else {
+                "Display"
+            },
+            self.min,
+            self.max,
+        );
 
         println!("{}", &message);
 

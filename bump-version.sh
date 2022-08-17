95f4001df729c247940b229f0d2f81176dee02cb
mbme <stribog.ua@gmail.com>
2022-08-17 11:50:33 +0300

    chore: improve bump-version script

--- bump-version.sh
+++ bump-version.sh
@@ -3,10 +3,12 @@
 CURRENT_VERSION=$(git describe --abbrev=0 --tags)
 NEXT_VERSION=$(($CURRENT_VERSION + 1))
 
-read -p "Are you sure you want to create new tag $NEXT_VERSION? (Y/n)" -n 1 -r
+read -p "Are you sure you want to create new tag $NEXT_VERSION? (y)" -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
   git tag $NEXT_VERSION
   git push --tags
+else
+  echo "Cancelled"
 fi

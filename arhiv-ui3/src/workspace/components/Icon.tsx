79db4b482d7b3585bbe960e2d4a1f394ea110c23
mbme <stribog.ua@gmail.com>
2022-08-26 22:01:45 +0300

    workspace: update icons

--- arhiv-ui3/src/workspace/components/Icon.tsx
+++ arhiv-ui3/src/workspace/components/Icon.tsx
@@ -6,6 +6,7 @@ export type IconVariant =
   | 'x'
   | 'clipboard'
   | 'clipboard-check'
+  | 'search'
   | 'search-catalog'
   | 'edit-document'
   | 'add-document'
@@ -34,8 +35,8 @@ export function Icon({ variant, className = '' }: IconProps) {
           strokeWidth="1.5"
         >
           <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeLinecap="round"
+            strokeLinejoin="round"
             d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18"
           />
         </svg>
@@ -77,13 +78,13 @@ export function Icon({ variant, className = '' }: IconProps) {
       );
 
     case 'clipboard':
-      // hero icons clipboard
+      // hero icons outline clipboard
       return (
         <svg
           className={cx('icon', className)}
           fill="none"
           viewBox="0 0 24 24"
-          stroke-width="1.5"
+          strokeWidth="1.5"
           stroke="currentColor"
         >
           <path
@@ -95,19 +96,37 @@ export function Icon({ variant, className = '' }: IconProps) {
       );
 
     case 'clipboard-check':
-      // hero icons clipboard-document-check
+      // hero icons outline clipboard-document-check
       return (
         <svg
           className={cx('icon', className)}
           fill="none"
           viewBox="0 0 24 24"
-          stroke-width="1.5"
+          strokeWidth="1.5"
           stroke="currentColor"
         >
           <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeLinecap="round"
+            strokeLinejoin="round"
             d="M11.35 3.836c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m8.9-4.414c.376.023.75.05 1.124.08 1.131.094 1.976 1.057 1.976 2.192V16.5A2.25 2.25 0 0118 18.75h-2.25m-7.5-10.5H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V18.75m-7.5-10.5h6.375c.621 0 1.125.504 1.125 1.125v9.375m-8.25-3l1.5 1.5 3-3.75"
+          />
+        </svg>
+      );
+
+    case 'search':
+      // hero icons outline magnifying-glass
+      return (
+        <svg
+          className={cx('icon', className)}
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
           />
         </svg>
       );

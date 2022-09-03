1cec8aaa058644ac3c1cce17c614c1a0ff749aa4
mbme <stribog.ua@gmail.com>
2022-09-03 14:45:12 +0300

    chore: disable strict typescript-eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,6 @@ module.exports = {
     'plugin:react-hooks/recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'plugin:@typescript-eslint/strict',
     'prettier',
   ],
   env: {

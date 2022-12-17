564690cca6cec6557ef0ab944e82c0fc687a5515
mbme <stribog.ua@gmail.com>
2022-12-17 10:40:07 +0200

    better error display

--- arhiv-ui/src/ui/components/QueryError.tsx
+++ arhiv-ui/src/ui/components/QueryError.tsx
@@ -4,7 +4,7 @@ type QueryErrorProps = {
 
 export function QueryError({ error }: QueryErrorProps) {
   return (
-    <pre className="bg-red-300 p-4">
+    <pre className="bg-red-300 p-4 whitespace-normal break-words">
       <code>{String(error)}</code>
     </pre>
   );

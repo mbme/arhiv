720f4b3014986493bc290ff1fb5ce1facbf0b263
mbme <stribog.ua@gmail.com>
2023-03-18 20:19:53 +0200

    remove unused tailwind plugin

--- arhiv-ui/tailwind.config.cjs
+++ arhiv-ui/tailwind.config.cjs
@@ -22,8 +22,7 @@ module.exports = {
     extend: {},
   },
   plugins: [
-    require('@tailwindcss/typography'),
+    require('@tailwindcss/typography'), //
     require('@tailwindcss/forms'),
-    require('@tailwindcss/line-clamp'),
   ],
 };

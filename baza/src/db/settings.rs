4e058970c7ceea3ecba0cd7deab08b48ff34aca7
mbme <stribog.ua@gmail.com>
2023-05-21 12:12:06 +0300

    refactor: use type InstanceId in the settings

--- baza/src/db/settings.rs
+++ baza/src/db/settings.rs
@@ -1,3 +1,5 @@
+use crate::sync::instance_id::InstanceId;
+
 use super::kvs::KvsConstKey;
 
 pub const SETTINGS_NAMESPACE: &'static str = "settings";
@@ -8,5 +10,5 @@ pub const SETTING_DATA_VERSION: &KvsConstKey<u8> =
 pub const SETTING_COMPUTED_DATA_VERSION: &KvsConstKey<u8> =
     &KvsConstKey::new(SETTINGS_NAMESPACE, "computed_data_version");
 
-pub const SETTING_INSTANCE_ID: &KvsConstKey<String> =
+pub const SETTING_INSTANCE_ID: &KvsConstKey<InstanceId> =
     &KvsConstKey::new(SETTINGS_NAMESPACE, "instance_id");

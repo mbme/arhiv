775d8dad9178e0c91d4396d3d9a13746b196f0e5
mbme <stribog.ua@gmail.com>
2023-09-21 13:50:08 +0300

    refactor: rename module

--- arhiv-ui/src/lib.rs
+++ arhiv-ui/src/lib.rs
@@ -7,9 +7,9 @@ pub mod definitions;
 mod dto;
 mod import;
 pub mod scraper;
-mod server;
 mod status;
+mod ui_server;
 
 pub use arhiv::{Arhiv, BazaConnectionExt};
 pub use config::Config;
-pub use server::build_ui_router;
+pub use ui_server::build_ui_router;

9439413df7f21428806681c2a7e58a9f2372586e
mbme <stribog.ua@gmail.com>
2023-12-20 15:43:15 +0200

    add simple android webview app

---
+++ arhiv-android/app/src/main/java/me/mbsoftware/arhiv/MainActivity.java
@@ -0,0 +1,31 @@
+package me.mbsoftware.arhiv;
+
+
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    // Find the WebView by its unique ID
+    WebView webView = findViewById(R.id.web);
+
+    // loading https://www.geeksforgeeks.org url in the WebView.
+    webView.loadUrl("https://www.geeksforgeeks.org");
+
+    // this will enable the javascript.
+    webView.getSettings().setJavaScriptEnabled(true);
+
+    // WebViewClient allows you to handle
+    // onPageFinished and override Url loading.
+    webView.setWebViewClient(new WebViewClient());
+  }
+}
+

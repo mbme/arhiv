b3a9094d4ef3370b6ad8bb7c819fe8d29b2d34b8
mbme <stribog.ua@gmail.com>
2024-02-22 20:49:22 +0200

    tweak Badge styles

--- arhiv/src/ui/components/Badge.tsx
+++ arhiv/src/ui/components/Badge.tsx
@@ -11,11 +11,12 @@ export function Badge({ label, checked, onClick, size = 'md', className }: Props
   return (
     <span
       className={cx(
-        'font-medium px-2.5 py-0.5 rounded-full border cursor-pointer select-none hover:bg-blue-100/40',
+        'font-medium px-2.5 py-0.5 rounded-full border select-none',
         {
           'bg-blue-100 text-blue-800 border-blue-400': checked,
           'text-xs': size === 'sm',
           'text-sm': size === 'md',
+          'cursor-pointer hover:bg-blue-100/40': Boolean(onClick),
         },
         className,
       )}

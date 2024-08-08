28b61eb954da06a1a7af5fb125225f9c830e0903
mbme <stribog.ua@gmail.com>
2024-08-08 21:56:08 +0300

    fix eslint error

--- arhiv/src/ui/Workspace/DocumentCard/DocumentCard.tsx
+++ arhiv/src/ui/Workspace/DocumentCard/DocumentCard.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { DocumentDTO } from 'dto';
 import { useUnsavedChangesWarning } from 'utils/hooks';
 import { RPC } from 'utils/rpc';

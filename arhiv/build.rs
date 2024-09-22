35c010c8f500c7a0c65a415c7270e822b99c90f6
mbme <stribog.ua@gmail.com>
2024-09-22 16:20:59 +0300

    prevent arhiv build script running all the time

--- arhiv/build.rs
+++ arhiv/build.rs
@@ -6,7 +6,6 @@ fn main() {
     println!("cargo:rerun-if-env-changed=PROFILE");
     println!("cargo:rerun-if-changed=src/ui");
     println!("cargo:rerun-if-changed=src/dto.ts");
-    println!("cargo:rerun-if-changed=public");
 
     if env::var("PROFILE").unwrap() == "release" {
         // build web app in release mode

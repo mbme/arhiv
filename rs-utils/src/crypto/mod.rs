d4a6c80fadfa3bf8c79ff2fcb180a17e80adaf61
mbme <stribog.ua@gmail.com>
2025-02-18 13:29:51 +0200

    refactor: lift age module

--- rs-utils/src/crypto/mod.rs
+++ rs-utils/src/crypto/mod.rs
@@ -1,12 +1,11 @@
+pub mod age;
 mod auth_token;
 mod certificate;
 pub mod crypto_key;
 mod hash;
 mod secret;
-mod stream;
 
 pub use auth_token::*;
 pub use certificate::*;
 pub use hash::*;
 pub use secret::*;
-pub use stream::*;

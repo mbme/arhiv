f1e0f728bc27324f502656175329467e520d2569
mbme <stribog.ua@gmail.com>
2025-03-08 10:09:53 +0200

    remove UI code for url attachments

--- arhiv/src/ui/components/Form/RefInput.tsx
+++ arhiv/src/ui/components/Form/RefInput.tsx
@@ -1,14 +1,13 @@
 import { useRef, useState } from 'react';
 import { cx, NominalType } from 'utils';
 import { useSuspenseQuery } from 'utils/suspense';
-import { ASSET_DOCUMENT_TYPE, DocumentId, DocumentType } from 'dto';
+import { DocumentId, DocumentType } from 'dto';
 import { Ref as RefComponent } from 'components/Ref';
 import { DocumentPicker } from 'components/DocumentPicker';
 import { Link } from 'components/Link';
 import { Button, IconButton } from 'components/Button';
 import { HTMLVFormFieldElement, FormField } from 'components/Form/FormField';
 import { AssetPreviewBlock, canPreview } from 'components/AssetPreview';
-import { AssetUrlDialog } from 'components/AssetUrlDialog';
 
 type AssetUrl = NominalType<string, 'AssetUrl'>;
 
@@ -61,7 +60,6 @@ export function RefInput({
   const [refs, setRefs] = useState(defaultValue);
 
   const [showDocumentPicker, setShowDocumentPicker] = useState(false);
-  const [showUrlPicker, setShowUrlPicker] = useState(false);
 
   const updateRefs = (newRefs: Ref[], triggerChange: boolean) => {
     if (newRefs.length > 1 && !multiple) {
@@ -89,7 +87,6 @@ export function RefInput({
   });
 
   const canAdd = refs.length === 0 || multiple;
-  const canAddUrls = documentTypes.includes(ASSET_DOCUMENT_TYPE) && canAdd;
 
   return (
     <FormField
@@ -165,22 +162,6 @@ export function RefInput({
         </Button>
       )}
 
-      {showUrlPicker && (
-        <AssetUrlDialog
-          onConfirm={(urlRaw) => {
-            const url = urlRaw as AssetUrl;
-
-            if (!refs.includes(url)) {
-              updateRefs([...refs, url], true);
-            }
-            setShowUrlPicker(false);
-          }}
-          onCancel={() => {
-            setShowUrlPicker(false);
-          }}
-        />
-      )}
-
       {refs.filter(isAssetUrl).map((assetUrl) => (
         <div key={assetUrl}>
           <div className="flex items-center gap-4">
@@ -201,20 +182,6 @@ export function RefInput({
           </div>
         </div>
       ))}
-
-      {canAddUrls && (
-        <Button
-          variant="text"
-          onClick={() => {
-            setShowUrlPicker(true);
-          }}
-          disabled={readonly || disabled}
-          busy={isUpdating}
-          leadingIcon="link"
-        >
-          Add url
-        </Button>
-      )}
     </FormField>
   );
 }
